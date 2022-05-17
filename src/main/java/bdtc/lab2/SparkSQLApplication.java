a58a629a4659db7aa68b953f46ff44fabb550b74
egor <EgorAdonev@github.com>
2022-05-17 14:37:17 -0700

    Application

--- src/main/java/bdtc/lab2/SparkSQLApplication.java
+++ src/main/java/bdtc/lab2/SparkSQLApplication.java
@@ -1,14 +1,17 @@
 package bdtc.lab2;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import scala.Tuple2;
 
 /**
- * Считает количество событий syslog разного уровная log level по часам.
+ * группа-среднее количество сообщений на одного пользователя в групп
  */
 @Slf4j
 public class SparkSQLApplication {
@@ -17,7 +20,7 @@ public class SparkSQLApplication {
      * @param args - args[0]: входной файл, args[1] - выходная папка
      */
     public static void main(String[] args) {
-        if (args.length < 2) {
+        if (args.length < 3) {
             throw new RuntimeException("Usage: java -jar SparkSQLApplication.jar input.file outputDirectory");
         }
 
@@ -28,11 +31,16 @@ public class SparkSQLApplication {
                 .master("local")
                 .appName("SparkSQLApplication")
                 .getOrCreate();
+        SparkConf sparkConf = new SparkConf();
+        sparkConf.setAppName("Spark RDD Example using Java");
+        sparkConf.setMaster("local[2]");
+        JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
+        JavaRDD<String> lines = sparkContext.textFile(args[0]);;
+        JavaRDD<String> lines2 = sparkContext.textFile(args[1]);
 
-        Dataset<String> df = sc.read().text(args[0]).as(Encoders.STRING());
         log.info("===============COUNTING...================");
-        JavaRDD<Row> result = AvgPerGroupUserCounter.countAvgPerGroupUser(df);
-        log.info("============SAVING FILE TO " + args[1] + " directory============");
-        result.saveAsTextFile(args[1]);
+        JavaRDD<Tuple2<String,Integer>> result = AvgPerGroupUserCounter.countAvgPerGroupUser(lines,lines2);
+        log.info("============SAVING FILE TO " + args[2] + " directory============");
+        result.saveAsTextFile(args[2]);
     }
 }
